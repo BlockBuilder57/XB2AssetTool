e45a92088948d71819924d9c00622555c5568bf3
modeco80 <lily.modeco80@protonmail.ch>
2020-11-07 16:33:08 -0600

    [Core] hopefully fix installation of xb2core?

--- include/xb2at/structs/msmd.h
+++ include/xb2at/structs/msmd.h
@@ -19,10 +19,15 @@ namespace xb2at {
 			 */
 			struct header {
 				uint32 magic;
-				byte unknown_padding[0x77]; // unknown padding data
-				uint32 table_offset;
+				byte unknown_padding[0x77]; // unknown padding data, this will be figured out later
+				uint32 table_offset; // this is duplicated at C8? 
+				// in ma02a 0x70 has what seems to be the true offset
+				// which in ma01a is the exact same as what's in 0x78 and c8 there
 			};
 
+			/**
+			 * msmd xbc1 table entry
+			 */
 			struct table_entry {
 				uint32 offset;
 				uint32 size;
